31b5ed49356432c6fcef92eb8eaba07d4a062ba7
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2017-04-04 17:59:56 +0200

    attempt one: span around match

--- routes/modules/buzzwords.js
+++ routes/modules/buzzwords.js
@@ -5,20 +5,43 @@ const buzzwords = {
     return new Promise(function(resolve, reject) {
       // turn each word into an array
       const words = message.split(' ');
+      let bla = false;
 
-      // filter the mached words from the search sentance
-      const matches = words.filter(word => {
+      const check = words.map(word => {
         let checker = false;
-        // check eacht item in an array like this, so the search isn't case sensative
-        fakeBuzzWordsHits.forEach(buzzWord => {
+
+        const matches = fakeBuzzWordsHits.forEach(buzzWord => {
           if (buzzWord.toUpperCase() === word.toUpperCase()) {
             checker = true;
+            bla = true;
           }
         });
-        return checker;
+        if (checker === true) {
+          return `<span>${word}</span>`;
+        } else {
+          return word;
+        }
       });
 
-      !matches ? reject('no matches') : resolve(matches);
+      console.log(check);
+      bla === true ? resolve(check) : reject('no matches');
+
+
+
+
+      // filter the mached words from the search sentance
+      // const matches = words.filter(word => {
+      //   let checker = false;
+      //   // check eacht item in an array like this, so the search isn't case sensative
+      //   fakeBuzzWordsHits.forEach(buzzWord => {
+      //     if (buzzWord.toUpperCase() === word.toUpperCase()) {
+      //       checker = true;
+      //     }
+      //   });
+      //   return checker;
+      // });
+      //
+      // !matches ? reject('no matches') : resolve(matches);
     });
   }
 };
