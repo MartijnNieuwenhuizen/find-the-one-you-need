081ea4aa6da3545be2a4d83deda06887a0e80c99
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2017-04-06 10:42:50 +0200

    lost an entire function

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,19 @@ const router = express.Router();
 
 const buzzwords = require('./modules/buzzwords');
 
-
-
-
-
-
-
 router.get('/', (req, res, next) => {
   const content = {};
   const message = req.query.message;
-  if (!message) { render(); }
-  content.message = message;
 
-    // Filter the right words from the message
-    // This is a demo function!!!!!
+  if (!message) { render(); }
+
+  content.message = message;
+    // Filter the right words from the message. This is a demo function!!!!!
     buzzwords.get(message)
       .then(matches => {
         content.buzzWords = matches;
+
+        // Search matches
         render();
       })
       .catch(err => {
