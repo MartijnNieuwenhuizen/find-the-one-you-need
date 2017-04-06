b8d0335e67012819f540a43d9bec9e13029d07ff
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2017-04-06 10:40:21 +0200

    changed POST to GET in the form

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,16 @@ const router = express.Router();
 
 const buzzwords = require('./modules/buzzwords');
 
+
+
+
+
+
+
 router.get('/', (req, res, next) => {
-  const content = {
-    test: 'test',
-  };
-  render();
-
-  function render() {
-    res.render('index', content);
-  }
-});
-
-
-
-
-
-
-router.post('/', (req, res, next) => {
   const content = {};
-  const message = req.body.message;
-  if (!message) { res.rederect('404'); }
+  const message = req.query.message;
+  if (!message) { render(); }
   content.message = message;
 
     // Filter the right words from the message
