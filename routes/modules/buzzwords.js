1c2bf51207b60d1d343d22724eb47704e9d205a3
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2017-04-10 15:05:59 +0200

    rewrite the code to modules with a Promise.all

--- routes/modules/buzzwords.js
+++ routes/modules/buzzwords.js
@@ -1,55 +1,40 @@
 const fakeBuzzWordsHits = ['nodeJS', 'javascript', 'Nuon', 'KLM', 'SASS', 'ES6'];
 
-const buzzwords = {
-  get: function(message) {
+class Buzzwords {
+  static create(message) {
     return new Promise(function(resolve, reject) {
-      // turn each word into an array
-      const words = message.split(' ');
-      let bla = false;
+      const wordsArray = message.split(' ');
 
-      const check = words.map(word => {
-        let checker = false;
+      const wordsArrayWithBuzzwords = wordsArray.map(Buzzwords.checkForBuzzword);
+      resolve(wordsArrayWithBuzzwords);
 
-        const matches = fakeBuzzWordsHits.forEach(buzzWord => {
-          if (buzzWord.toUpperCase() === word.toUpperCase()) {
-            checker = true;
-            bla = true;
-          }
-        });
-        if (checker === true) {
-          return {
-            word,
-            match: true,
-            type: 'development' // @TODO: make this dynamic
-          };
-        } else {
-          return {
-            word,
-            matche: false
-          };
-        }
-      });
-
-      bla === true ? resolve(check) : reject('no matches');
-
-
-
-
-      // filter the mached words from the search sentance
-      // const matches = words.filter(word => {
-      //   let checker = false;
-      //   // check eacht item in an array like this, so the search isn't case sensative
-      //   fakeBuzzWordsHits.forEach(buzzWord => {
-      //     if (buzzWord.toUpperCase() === word.toUpperCase()) {
-      //       checker = true;
-      //     }
-      //   });
-      //   return checker;
-      // });
-      //
-      // !matches ? reject('no matches') : resolve(matches);
     });
   }
-};
 
-module.exports = buzzwords;
+  static checkForBuzzword(word) {
+    const comparison = Buzzwords.compareWords(word);
+
+    if (comparison === true) {
+      return {
+        word,
+        match: true,
+        type: 'development' // @TODO: make this dynamic
+      };
+    } else {
+      return {
+        word,
+        matche: false
+      };
+    }
+  }
+
+  static compareWords(word) {
+    let isABuzzword = false;
+    fakeBuzzWordsHits.forEach(buzzWord => {
+      if (buzzWord.toUpperCase() === word.toUpperCase()) { isABuzzword = true; }
+    });
+    return isABuzzword;
+  }
+}
+
+module.exports = Buzzwords;
