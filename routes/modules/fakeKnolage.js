2f30b2612a1829af8d93d1cda73a593b9d10a49b
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2017-04-13 15:45:26 +0200

    reordered the fake data into two files

---
+++ routes/modules/fakeKnolage.js
@@ -0,0 +1,99 @@
+module.exports = [
+  {
+    id: 1000,
+    areas: [
+      {
+        name: 'Front-end',
+        subArea: [
+          {
+            name: 'CSS',
+            points: [
+              {
+                name: 'CSS Grid',
+              }
+            ]
+          }
+        ]
+      },
+    ],
+    projects: [
+      {
+        name: 'Nuon',
+        role: 'Front-end Developer',
+        assignments: ['Login Scherm', 'Chatbot Logica', 'Vertalingen', 'Footer 2.0']
+      },
+      {
+        name: 'KLM',
+        role: 'Front-end Developer',
+        assignments: ['Login Scherm', 'Chatbot Logica', 'Vertalingen', 'Footer 2.0']
+      }
+    ]
+  },
+  {
+    id: 1001,
+    areas: [
+      {
+        name: 'Front-end',
+        subArea: [
+          {
+            name: 'CSS',
+            points: [
+              {
+                name: 'CSS Grid',
+              }
+            ]
+          }
+        ]
+      },
+    ],
+    projects: [
+      {
+        name: 'Nuon',
+        role: 'Front-end Developer',
+        assignments: ['Chatbot berichten', 'Menu']
+      },
+      {
+        name: 'KLM',
+        role: 'Front-end Developer',
+        assignments: ['Chatbot berichten', 'Menu']
+      },
+      {
+        name: 'Fleurop',
+        role: 'Front-end Developer',
+        assignments: ['Chatbot berichten', 'Menu']
+      }
+    ]
+  },
+  {
+    id: 1002,
+    areas: [
+      {
+        name: 'Front-end',
+        subArea: [
+          {
+            name: 'CSS',
+            points: [
+              {
+                name: 'CSS Grid',
+              }
+            ]
+          }
+        ]
+      },
+    ],
+    projects: [
+      {
+        name: 'Nuon',
+        role: 'Front-end Developer',
+        assignments: ['Login Scherm', 'Chatbot Logica', 'Vertalingen', 'Footer extended'],
+        hoursWorked: 30
+      },
+      {
+        name: 'Transavia',
+        role: 'Front-end Developer',
+        assignments: ['Login Scherm', 'Chatbot Logica', 'Vertalingen', 'Footer extended'],
+        hoursWorked: 121
+      }
+    ]
+  }
+];
---
+++ routes/modules/fakePeople.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    name: {
+      first: 'Martijn',
+      last: 'Nieuwenhuizen'
+    },
+    role: 'Front-end Developer',
+    location: 'Amsterdam',
+    currentLocation: 'Amsterdam',
+    knolageId: 1000
+  },
+  {
+    name: {
+      first: 'Sandy',
+      last: 'Smith'
+    },
+    role: 'Front-end Developer',
+    location: 'Eindhoven',
+    currentLocation: 'Amsterdam',
+    knolageId: 1001
+  },
+  {
+    name: {
+      first: 'Bjurn',
+      last: 'Kuipers'
+    },
+    role: 'Senior Front-end Developer',
+    location: 'Hoorn',
+    currentLocation: 'Rotterdam',
+    knolageId: 1002
+  }
+];
